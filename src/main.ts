49193145ab9811af6852f1ace0285fc6cd4c57cf
Davide Pedron <davide@happydev.cloud>
2020-09-18 22:50:59 +0200

    Update asset and position on screen

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,26 @@ import Phaser from 'phaser'
 
 import MainScene from './scenes/MainScene'
 
+export const DEFAULT_WIDTH = 1920
+export const DEFAULT_HEIGHT = 1080
+
 const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
-    width: 960,
-    height: 540,
+    scale: {
+        parent: 'phaser-game',
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: DEFAULT_WIDTH,
+        height: DEFAULT_HEIGHT
+    },
     physics: {
         default: 'arcade',
         arcade: {
             gravity: { y: 600 }
         }
+    },
+    render: {
+        pixelArt: true
     },
     scene: [MainScene]
 }
--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -1,4 +1,5 @@
 import Phaser from 'phaser'
+import {DEFAULT_HEIGHT, DEFAULT_WIDTH} from "~/main";
 
 export default class MainScene extends Phaser.Scene
 {
@@ -21,16 +22,9 @@ export default class MainScene extends Phaser.Scene
 
     create()
     {
-        this.add.image(480, 270, 'background').setScale(1.6,1.6)
+        this.add.image(DEFAULT_WIDTH/2, DEFAULT_HEIGHT/2, 'background').setScale(1.6,1.6)
 
-        const particles = this.add.particles('red')
-        this.emitter = particles.createEmitter({
-            speed: 70,
-            scale: { start: 1, end: 0 },
-            blendMode: Phaser.BlendModes.ADD
-        })
-
-        this.arrow = this.add.image(100, 250, 'arrow').setScale(0.4, 0.4)
+        this.arrow = this.add.image(DEFAULT_WIDTH*0.15, DEFAULT_HEIGHT*0.6, 'arrow').setScale(0.4, 0.4)
     }
 
     update(time: number, delta: number) {
@@ -44,12 +38,21 @@ export default class MainScene extends Phaser.Scene
         this.input.on('pointerdown', (pointer) => {
             const cursor = pointer;
 
-            if (!this.toast) {
-                this.toast = this.physics.add.image(100, 250, 'toast').setScale(0.4, 0.4)
+            if (!this.toast || (this.toast.body.velocity.y == 0 && this.toast.body.velocity.x == 0)) {
+                const particles = this.add.particles('red')
+                this.emitter = particles.createEmitter({
+                    speed: 70,
+                    scale: { start: 1, end: 0 },
+                    blendMode: Phaser.BlendModes.ADD
+                })
+                if (this.toast) {
+                    this.toast.destroy()
+                }
+                this.toast = this.physics.add.image(this.arrow.x, this.arrow.y, 'toast').setScale(0.4, 0.4)
                 this.toast.setCollideWorldBounds(true, 0, 0)
                 this.toast.setAngularVelocity(200)
 
-                this.physics.moveTo(this.toast, cursor.x, cursor.y, 600);
+                this.physics.moveTo(this.toast, cursor.x, cursor.y, 800);
 
                 this.emitter.startFollow(this.toast)
             }
