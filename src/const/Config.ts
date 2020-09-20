58b85d1e81a98541be6153e1a177526fab9ef66d
Davide Pedron <davide@happydev.cloud>
2020-09-20 15:56:07 +0200

    Change time of spinnig time

--- src/const/Config.ts
+++ src/const/Config.ts
@@ -1,5 +1,5 @@
 export const BGM_VOLUME = 0.2
 export const TOAST_SCALE = 0.15
-export const SPEED_EFFECT_TIME = 8000
+export const SPEED_EFFECT_TIME = 6500
 export const DEFAULT_TOSS_SPEED = 1100
 export const TIME_TO_ENTER_SPEED_EFFECT = 900
