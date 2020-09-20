2a5d98f8ff82f193a5f3e0448087be1efb7e1def
Davide Pedron <davide@happydev.cloud>
2020-09-20 05:50:10 +0200

    Change toast size

--- src/objects/Toast.ts
+++ src/objects/Toast.ts
@@ -1,5 +1,6 @@
 import Phaser from 'phaser'
 import MainScene from '~/scenes/MainScene'
+import {TOAST_SCALE} from "~/const/Config";
 
 export default class Toast extends Phaser.Physics.Arcade.Sprite {
     readonly animName: string;
@@ -12,7 +13,7 @@ export default class Toast extends Phaser.Physics.Arcade.Sprite {
         this.textureName = data.texture
         scene.add.existing(this)
 
-        this.setScale(0.20, 0.20)
+        this.setScale(TOAST_SCALE, TOAST_SCALE)
     }
 
     toss(cursorX: number, cursorY: number) {
