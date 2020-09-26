eb6ecc5306322dab7d4872ac7b4e6207b5f1319b
Davide Pedron <davide@happydev.cloud>
2020-09-26 22:04:58 +0200

    fix collision position

--- src/objects/BarCounter.ts
+++ src/objects/BarCounter.ts
@@ -5,12 +5,12 @@ import {DEFAULT_HEIGHT, DEFAULT_WIDTH} from '~/main'
 
 export default class BarCounter extends Phaser.Physics.Arcade.Sprite {
     constructor(scene: MainScene) {
-        super(scene, DEFAULT_WIDTH*0.765, DEFAULT_HEIGHT*0.8, PLATE)
+        super(scene, DEFAULT_WIDTH*0.74, DEFAULT_HEIGHT*0.78, PLATE)
 
         this.setVisible(false)
         scene.add.existing(this)
         scene.physics.add.existing(this, true)
 
-        this.setSize(260, 400)
+        this.setSize(200, 300)
     }
 }
--- src/objects/Floor.ts
+++ src/objects/Floor.ts
@@ -5,12 +5,12 @@ import {DEFAULT_HEIGHT, DEFAULT_WIDTH} from '~/main'
 
 export default class Floor extends Phaser.Physics.Arcade.Sprite {
     constructor(scene: MainScene) {
-        super(scene, DEFAULT_WIDTH*0.5, DEFAULT_HEIGHT*0.80, PLATE)
+        super(scene, DEFAULT_WIDTH*0.5, DEFAULT_HEIGHT*0.87, PLATE)
 
         this.setVisible(false)
         scene.add.existing(this)
         scene.physics.add.existing(this, true)
 
-        this.setSize(DEFAULT_WIDTH, 50)
+        this.setSize(DEFAULT_WIDTH*1.5, 50)
     }
 }
